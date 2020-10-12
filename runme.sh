1e3654a1be593281a97edf2f24782199dbc722ab
Gabriel PATINGRE <gpatingr@e3r3p3.42.fr>
2020-10-12 18:52:00 +0200

    testing compilation, everithing ok

--- runme.sh
+++ runme.sh
@@ -5,6 +5,10 @@ function cleanup()
 	rm -f diff.txt
 }
 trap cleanup EXIT
-i=1
+src_path="../gnl"
+buffer_size=1
 echo -n "" > DEEPTHOUGHT
 
+while [ buffer_size -le 8 ]
+do
+	gcc -Wall -Werror -Wextra -D BUFFER_SIZE=$(buffer_size) $(src_path)/get_next_line.c $(src_path)/get_next_line_utils.c -o tests/$(buffer_size)
