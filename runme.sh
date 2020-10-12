c788efaf28b51cafebe85b0437a118097a96196a
Gabriel PATINGRE <gpatingr@e3r3p3.42.fr>
2020-10-12 18:57:49 +0200

    moving mains

--- runme.sh
+++ runme.sh
@@ -9,6 +9,9 @@ src_path="../gnl"
 buffer_size=1
 echo -n "" > DEEPTHOUGHT
 
+#normal mode
 while [ buffer_size -le 8 ]
 do
-	gcc -Wall -Werror -Wextra -D BUFFER_SIZE=$(buffer_size) $(src_path)/get_next_line.c $(src_path)/get_next_line_utils.c -o tests/$(buffer_size)
+	gcc -Wall -Werror -Wextra -D BUFFER_SIZE=$(buffer_size) $(src_path)/get_next_line.c $(src_path)/get_next_line_utils.c mains/main.c -o tests/$(buffer_size).out
+	((buffer_size++))
+done
